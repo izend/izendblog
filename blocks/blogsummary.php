52ee35cef4070305f2be931ded7715c17ded98c3
izend <izend@izend.org>
2013-01-29 00:04:39 +0100

    Corrects how link to main article is checked

--- blocks/blogsummary.php
+++ blocks/blogsummary.php
@@ -3,11 +3,9 @@
 /**
  *
  * @copyright  2010-2013 izend.org
- * @version    6
+ * @version    7
  * @link       http://www.izend.org
  */
-
-require_once 'nodethread.php';
 
 require_once 'models/blog.inc';
 
@@ -34,21 +32,21 @@ function blogsummary($lang, $blog_id, $taglist=false, $pagesize=false, $page=1) 
 	$index=false;
 	$flat=true;
 
-	$blogsummary = array();
+	$blogsummary=array();
 	foreach ($nodelist as $node) {
 		extract($node);
-		$author = $user_name;
-		$title = $node_title ? $node_title : $node_id;
-		$uri = nodethread($lang, $node_name) ? $lang . '/' . $node_name : false;
-		$created = $node_created;
-		$modified = $node_modified;
-		$abstract = $node_abstract;
-		$author = $user_name;
-		$summary = build('nodecontent', $lang, $node_id);
+		$author=$user_name;
+		$title=$node_title ? $node_title : $node_id;
+		$created=$node_created;
+		$modified=$node_modified;
+		$abstract=$node_abstract;
+		$author=$user_name;
+		$summary=build('nodecontent', $lang, $node_id);
 		$cloud=build('cloud', $lang, $cloud_url, $blog_id, $node_id, 10, compact('inclusive', 'byname', 'bycount', 'index', 'flat'));
 		$id=blog_node($node_name, $lang);
+		$uri=$id ? $lang . '/' . $node_name : false;
 		$vote=$id ? build('vote', $lang, $id, 'node', false) : false;
-		$blogsummary[] = compact('author', 'title', 'uri', 'created', 'modified', 'abstract', 'cloud', 'summary', 'vote');
+		$blogsummary[]=compact('author', 'title', 'uri', 'created', 'modified', 'abstract', 'cloud', 'summary', 'vote');
 	}
 
 	$output = view('blogsummary', $lang, compact('blogsummary', 'taglist', 'count', 'page', 'pagesize'));
