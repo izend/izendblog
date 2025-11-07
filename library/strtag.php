63e9936cca5572d5a043f3629ce1ac0a1a365888
izend <izend@izend.org>
2025-11-07 18:25:12 +0100

    Blurs text in strtag

--- library/strtag.php
+++ library/strtag.php
@@ -2,8 +2,8 @@
 
 /**
  *
- * @copyright  2010-2012 izend.org
- * @version    2
+ * @copyright  2010-2025 izend.org
+ * @version    3
  * @link       http://www.izend.org
  */
 
@@ -25,12 +25,18 @@ function strtag($text) {
 
 	imagefill($img, 0, 0, $bg);
 
-	// print text unevenly
+	// print text unevenly in a random color
 	for ($x=15, $i=0; $i<$len; $i++) {
 		$y = rand($h/2,$h/2+15);
 		$r = rand(-45, 45);
+		$fg=imagecolorallocate($img, rand(32, 128), rand(32, 128), rand(32, 128));
 		imagettftext($img, $fontsize, $r, $x, $y, $fg, $fontfile, $text[$i]);
 		$x += rand(25, 35);
+	}
+
+	// blur
+	for ($i=0; $i<4; $i++) {
+		imagefilter($img, IMG_FILTER_GAUSSIAN_BLUR);
 	}
 
 	// blur with colored dots
@@ -41,4 +47,3 @@ function strtag($text) {
 
 	return $img;
 }
-
