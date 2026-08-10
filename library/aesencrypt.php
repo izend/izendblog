0042f8f21a788d24597d67215c2fa446ebaa51d5
izend <izend@izend.org>
2026-08-10 12:45:10 +0200

    Replaces openssl_random_pseudo_bytes by random_bytes in aesencrypt in library

--- library/aesencrypt.php
+++ library/aesencrypt.php
@@ -2,24 +2,28 @@
 
 /**
  *
- * @copyright  2013-2020 izend.org
- * @version    4
+ * @copyright  2013-2026 izend.org
+ * @version    5
  * @link       http://www.izend.org
  */
 
 function aesencrypt($s, $key) {
 	$cipher = 'aes-256-cbc';
 	$iv_size = openssl_cipher_iv_length($cipher);
-	$iv = openssl_random_pseudo_bytes($iv_size);
+	$iv = random_bytes($iv_size);
 
 	$crypto = @openssl_encrypt($s, $cipher, $key, 0, $iv);
 
-	return $iv . $crypto;
+	return $crypto ? $iv . $crypto : false;
 }
 
 function aesdecrypt($s, $key) {
 	$cipher = 'aes-256-cbc';
 	$iv_size = openssl_cipher_iv_length($cipher);
+
+	if (strlen($s) < $iv_size)
+		return false;
+
 	$iv = substr($s, 0, $iv_size);
 
 	$crypto = substr($s, $iv_size);
