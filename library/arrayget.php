7c12dd55b83aca67df345763a0be0ab1862cfb6c
izend <izend@izend.org>
2026-08-11 16:16:18 +0200

    Makes sure $keys argument is an array and not empty in array_get and array_put in library

--- library/arrayget.php
+++ library/arrayget.php
@@ -3,11 +3,14 @@
 /**
  *
  * @copyright  2022-2026 izend.org
- * @version    2
+ * @version    3
  * @link       http://www.izend.org
  */
 
 function array_get($v, $keys) {
+	if (!is_array($keys) || empty($keys))
+		return false;
+
 	foreach ($keys as $k) {
 		if (!is_array($v) || !array_key_exists($k, $v))
 			return false;
