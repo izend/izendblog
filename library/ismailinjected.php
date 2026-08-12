1dbeb1ceaa9d175b27685dae7304ce496fe8e661
izend <izend@izend.org>
2026-08-13 01:59:51 +0200

    Rewrites is_mail_injected in library

--- library/ismailinjected.php
+++ library/ismailinjected.php
@@ -2,24 +2,11 @@
 
 /**
  *
- * @copyright  2010-2011 izend.org
- * @version    1
+ * @copyright  2010-2026 izend.org
+ * @version    2
  * @link       http://www.izend.org
  */
 
 function is_mail_injected($s) {
-	$injections = array(
-	'(\n+)',
-	'(\r+)',
-	'(\t+)',
-	'(%0A+)',
-	'(%0D+)',
-	'(%08+)',
-	'(%09+)'
-	);
-	$reg = implode('|', $injections);
-	$reg = "/$reg/";
-
-	return preg_match($reg, $s);
+    return preg_match('/[\r\n\t]|%0a|%0d|%08|%09/i', $s) == 1;
 }
-
