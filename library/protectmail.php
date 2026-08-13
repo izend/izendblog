a9af83e09a17a0776195c78b8717fb992f516d49
izend <izend@izend.org>
2026-08-13 17:21:16 +0200

    Rewrites protect_mail in library

--- library/protectmail.php
+++ library/protectmail.php
@@ -2,24 +2,12 @@
 
 /**
  *
- * @copyright  2010-2011 izend.org
- * @version    1
+ * @copyright  2010-2026 izend.org
+ * @version    2
  * @link       http://www.izend.org
  */
 
 function protect_mail($s) {
-	$injections = array(
-	'(\n+)',
-	'(\r+)',
-	'(\t+)',
-	'(%0A+)',
-	'(%0D+)',
-	'(%08+)',
-	'(%09+)'
-	);
-	$reg = implode('|', $injections);
-	$reg = "/$reg/";
-
-	return preg_replace($reg, '' ,$s);
+	return preg_replace('/[\r\n\t]|%0a|%0d|%08|%09/i', '', $s);
 }
 
