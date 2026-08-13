4be56d1184031bb9e1698bd3f3db552b83eae2bf
izend <izend@izend.org>
2026-08-13 16:37:14 +0200

    Removes unnecessary call to srand() in strrand in library

--- library/strrand.php
+++ library/strrand.php
@@ -2,16 +2,14 @@
 
 /**
  *
- * @copyright  2010-2011 izend.org
- * @version    1
+ * @copyright  2010-2026 izend.org
+ * @version    2
  * @link       http://www.izend.org
  */
 
 function strrand($charset, $len) {
 	$max=strlen($charset)-1;
 	$s = '';
-
-	srand();
 
 	for ($i=0; $i < $len; $i++) {
 		$s .= $charset[rand(0, $max)];
